8e2ff5e6f48f5615b8d88f572092af38d9a01120
mouradsme <mouradsme@gmail.com>
2024-05-20 16:36:23 +0100

    Ups

--- static/settings.py
+++ static/settings.py
@@ -140,7 +140,8 @@ USE_TZ = True
 STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR,'staticfiles')
 STATICFILES_DIRS = [
-        os.path.join(BASE_DIR,'static')
+        BASE_DIR / "static",
+        "/workspace/static/"
 
 ]
 
